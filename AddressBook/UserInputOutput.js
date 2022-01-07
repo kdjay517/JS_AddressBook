1c3781d6b7cac5b841b803fe5cc3f59ecd59ce4a
kdjay517 <kdjay517@gmail.com>
2022-01-07 06:22:59 +0530

    Added: searching contact by city or state

--- AddressBook/UserInputOutput.js
+++ AddressBook/UserInputOutput.js
@@ -5,8 +5,9 @@ class UserInputOutput{
         console.log('press 2 - to display contact details');
         console.log('press 3 - to edit the contact by first name');
         console.log('press 4 - to delete the contact by first name');
-        console.log('press 5 - to get count of contacts')
-        console.log('press 6 - to  EXIT');
+        console.log('press 5 - to get count of contacts');
+        console.log('press 6 - to serach contact by city or state');
+        console.log('press 7 - to  EXIT');
         return parseInt(prompt('enter option'));
     }
 }
